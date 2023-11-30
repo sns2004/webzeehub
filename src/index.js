5bd60367a402c649e94eaa9e4e37d3c5f07a5683
Shanjai <shanjaikumaarsn@gmail.com>
2023-11-30 14:41:43 +0530

    First webs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 const router = createHashRouter([
   {
