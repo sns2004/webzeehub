ef6f1cad45911a4d93124f9f6b9f6e5892d43567
Shanjai <shanjaikumaarsn@gmail.com>
2023-11-30 14:50:17 +0530

    First webs

--- src/index.js
+++ src/index.js
@@ -2,21 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {
-  createHashRouter,
-  RouterProvider
-} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
