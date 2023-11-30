261b6d159b7e01a9adf20fd16b1533740f9843d1
Shanjai <shanjaikumaarsn@gmail.com>
2023-11-30 14:40:42 +0530

    First webs

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
-
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
-        <App />
-    </HashRouter>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
