c252711865f4652f8d0677cdbd0e1ef36b4a065e
Shanjai <shanjaikumaarsn@gmail.com>
2023-11-30 14:06:21 +0530

    First websi

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const About=()=>{
          <h1 className="text-primary text-[28px] my-4 font-semibold">ABOUT US</h1>
          <div className="about-content w-full flex text-white  justify-center">
               <div className="about-left-content w-1/2">
-                <img src="/images/About-us-page.png" className="h-[400px] w-full"alt="#"/>
+                <img src="/webzee/images/About-us-page.png" className="h-[400px] w-full"alt="#"/>
               </div>
               <div  className="about-right-content w-1/2 relative top-16 px-3">
                 <p className="text-2xl  font-light text-justify">Our freelancing business specializes in the dynamic fields of web and graphic design, offering a comprehensive suite of services tailored to your creative needs. Whether you require a stunning website, a captivating logo, or eye-catching branding materials, we've got you covered.</p>
